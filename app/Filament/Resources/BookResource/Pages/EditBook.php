3f7d5143b4a79a56fd670e681172e4e38e5d8e39
christmex <christmex@yahoo.com>
2023-08-12 12:53:01 +0700

    add redirect after perform action

--- app/Filament/Resources/BookResource/Pages/EditBook.php
+++ app/Filament/Resources/BookResource/Pages/EditBook.php
@@ -16,4 +16,9 @@ class EditBook extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
